0ae85fe6eaa7ea3496ccc62ba5fc89818eae7007
wonged6 <eddie.wong@oocl.com>
2024-11-21 14:28:45 +0800

    feat: finish step 3

--- src/main/java/oo/Klass.java
+++ src/main/java/oo/Klass.java
@@ -1,4 +1,28 @@
 package oo;
 
+import java.util.Objects;
+
 public class Klass {
+    private Integer number;
+
+    public Klass(Integer number) {
+        this.number = number;
+    }
+
+    public Integer getNumber() {
+        return number;
+    }
+
+    public void setNumber(Integer number) {
+        this.number = number;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null || this.getClass() != obj.getClass()) {
+            return false;
+        }
+        Klass klass = (Klass) obj;
+        return this.number.equals(klass.number);
+    }
 }
--- src/main/java/oo/Student.java
+++ src/main/java/oo/Student.java
@@ -1,9 +1,29 @@
 package oo;
 
+import java.util.Optional;
+
 public class Student extends Person {
     private static final String ROLE = "student";
+    private Optional<Klass> klass = Optional.empty();
 
     public Student(final int id, final String name, final int age) {
         super(id, name, age, ROLE);
+    }
+
+    @Override
+    public String introduce() {
+        String introduction = super.introduce();
+        if (this.klass.isEmpty()) {
+            return introduction;
+        }
+        return String.format("%s I am in class %d.", introduction, klass.get().getNumber());
+    }
+
+    public void join(final Klass klass) {
+        this.klass = Optional.of(klass);
+    }
+
+    public boolean isIn(final Klass klass) {
+        return this.klass.map(value -> value.equals(klass)).orElse(false);
     }
 }
