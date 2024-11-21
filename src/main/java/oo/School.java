28010af60df65a7a28280bbdfb4a221c157cf7b4
wonged6 <eddie.wong@oocl.com>
2024-11-21 21:13:34 +0800

    feat: finish step 7

--- src/main/java/oo/School.java
+++ src/main/java/oo/School.java
@@ -1,4 +1,20 @@
 package oo;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class School {
+    private final List<Person> people = new ArrayList<Person>();
+
+    public void startSchool() {
+        people.forEach(person -> {
+            System.out.println(person.introduce());
+        });
+    }
+
+    public void addPerson(Person person) {
+        if (!people.contains(person)) {
+            people.add(person);
+        }
+    }
 }
