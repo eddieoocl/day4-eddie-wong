ab4bd6cb849a1e60e7adf8bf9c42097a4c1a4ce3
wonged6 <eddie.wong@oocl.com>
2024-11-21 13:48:48 +0800

    feat: finish step 1

--- src/main/java/oo/Person.java
+++ src/main/java/oo/Person.java
@@ -1,4 +1,26 @@
 package oo;
 
 public class Person {
+    private final Integer id;
+    private final String name;
+    private final Integer age;
+
+    public Person(final int id, final String name, final int age) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+    }
+
+    public String introduce() {
+        return String.format("My name is %s. I am %d years old.", name, age);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this.getClass() != obj.getClass()) {
+            return false;
+        }
+        Person person = (Person) obj;
+        return this.id.equals(person.id);
+    }
 }
